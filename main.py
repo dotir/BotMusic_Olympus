28daa6e8f3cba5d67531e257ca58c05628861068
Irving <irvingllerena@gmail.com>
2024-01-23 22:26:13 -0500

    solucion aparentemente del stop

--- main.py
+++ main.py
@@ -87,6 +87,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
     def __str__(self):
         return '**{0.title}** by **{0.uploader}**'.format(self)
 
+    def stop(self):
+        """Detiene la reproducción actual y cierra los recursos asociados."""
+        if self.channel:
+            self.channel.stop()
+            self.channel.cleanup()
+            self.channel = None
+
+
     @classmethod
     async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
         loop = loop or asyncio.get_event_loop()
@@ -278,6 +286,9 @@ class VoiceState:
     async def stop(self):
         self.songs.clear()
 
+        if self.is_playing:
+            self.voice.stop()
+
         if self.voice:
             await self.voice.disconnect()
             self.voice = None
@@ -405,6 +416,8 @@ class Music(commands.Cog):
         """Stops playing song and clears the queue."""
 
         ctx.voice_state.songs.clear()
+        await ctx.voice_state.stop()
+        await ctx.message.add_reaction('⏹')
 
         if  ctx.voice_state.is_playing:
             ctx.voice_state.voice.stop()
