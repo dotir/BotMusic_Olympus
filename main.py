a58c61f3967ba374f3e488435a32857a622acabb
Irving <irvingllerena@gmail.com>
2024-01-30 11:24:15 -0500

    prueba 1

--- main.py
+++ main.py
@@ -194,14 +194,9 @@ class Song:
         self.requester = source.requester
 
     def create_embed(self):
-        embed = (discord.Embed(title='Now playing',
+        embed = (discord.Embed(title='Ahora reproduciendo',
                                description='```css\n{0.source.title}\n```'.format(self),
-                               color=discord.Color.blurple())
-                 .add_field(name='Duration', value=self.source.duration)
-                 .add_field(name='Requested by', value=self.requester.mention)
-                 .add_field(name='Uploader', value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
-                 .set_thumbnail(url=self.source.thumbnail))
+                               color=discord.Color.blurple()))
 
         return embed
 
