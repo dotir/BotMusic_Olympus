6480ddf17fdc124d1c501c2a3340f17d99136325
Irving <irvingllerena@gmail.com>
2024-01-30 17:34:12 -0500

    quite el tiempo

--- main.py
+++ main.py
@@ -276,8 +276,8 @@ class VoiceState:
 
             if not self.loop:
                 try:
-                    async with timeout(180):
-                        self.current = await self.songs.get()
+                    #async with timeout(180):
+                    self.current = await self.songs.get()
                 except asyncio.TimeoutError:
                     self.bot.loop.create_task(self.stop())
                     return
